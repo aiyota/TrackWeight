41bba45f9ebd3b6722b2904271964583f4086520
Aaron Y <aiyota@outlook.com>
2023-05-16 06:11:45 -0700

    Create Weight model.

---
+++ TrackWeight.Api/Models/Weight.cs
@@ -0,0 +1,9 @@
+﻿namespace TrackWeight.Api.Models;
+
+public class Weight
+{
+    public int Id { get; set; }
+    public string UserId { get; set; } = default!;
+    public double Amount { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
