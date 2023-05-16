91b4f63120b31cada4444429fb28c5ac7dfd3fc3
Aaron Y <aiyota@outlook.com>
2023-05-16 06:08:58 -0700

    Create User model.

---
+++ TrackWeight.Api/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace TrackWeight.Api.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public string PasswordHash { get; set; } = default!;
+    public DateTime CreatedAt { get; set; }
+    public DateTime ModifiedAt { get; set; }
+}
